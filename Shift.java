2df77f7ab2d1d9b2dee2cb8513ba1f8f7efce0da
venkatsai963 <venkatgunji298@gmail.com>
2025-01-31 00:02:13 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,8 @@
+public class Shift {
+    public static void main(String[] args) {
+        int a=5;
+        int b=1;
+        System.out.println(a<<b);
+        System.out.println(a>>b);
+    }
+}
